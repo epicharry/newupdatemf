51b7c161a749d61cb0478eec510e32f64674f9f6
Harry <38373650+epicharry@users.noreply.github.com>
2025-09-10 07:57:59 +0530

    Remove Companion App Text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -114,11 +114,6 @@ export const Header: React.FC<HeaderProps> = ({
               }`}>
                 Welcome {getUserDisplayName()}
               </p>
-              <p className={`text-sm ${
-                isDarkMode ? 'text-gray-400' : 'text-gray-600'
-              }`}>
-                Your Valorant companion app
-              </p>
             </div>
           </div>
         </div>
