1d2db999bc1da1c25887e44d19bd418604a21793
Harry <38373650+epicharry@users.noreply.github.com>
2025-09-11 09:04:00 +0530

    Fix syntax error in playerSearchAPI.ts

--- src/services/playerSearchAPI.ts
+++
@@ -1,5 +0,0 @@
-Fetched rank for borne#5389: {tier: 0, rr: 0, rank: 'Unranked'}
-MatchHistoryPage.tsx:48 Using player's region: na for match history
-matchHistoryAPI.ts:509 Creating region-specific API for na to fetch match history for FgBrFqyna2JvYSl8dXHiZ9m48GuqRrNRJt9u0b49L5YCSirEkp67kTW96ynUuK_xRgIsLBZ9qH4sog
-MatchHistoryPage.tsx:48 Using player's region: na for match history
-matchHistoryAPI.ts:509 Creating region-specific API for na to fetch match history for FgBrFqyna2JvYSl8dXHiZ9m48GuqRrNRJt9u0b49L5YCSirEkp67kTW96ynUuK_xRgIsLBZ9qH4sog
